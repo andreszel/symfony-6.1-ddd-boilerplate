f698fdb2446a74ead21eaa3c08d999fed29c9b9c
Andrzej Szelka <szelkaandrzej@gmail.com>
2022-12-12 02:11:45 +0100

    Install packages to test, add 2 tests

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testHomepageHeaderText(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Gym System Pro ver. 2.0');
+    }
+
+    public function testVisitingWhileLoggedIn()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('szelkaandrzej@gmail.com');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h3', 'AUTHENTICATED OK - You are logged as ' . $testUser->getEmail());
+    }
+}
